c5a937ddf5f27980b4cf98059a4b8a7903beaf66
johncreed <hsiajuiyang5174@gmail.com>
2020-06-23 16:09:59 +0800

    Add folders

--- additional_lambda_parameter_range/kdd12/grid.sh
+++ additional_lambda_parameter_range/kdd12/grid.sh
@@ -14,7 +14,7 @@ task(){
         for w in 0.00048828125 0.0009765625 0.001953125 0.00390625 0.0078125 0.015625 0.03125 0.0625 0.125 0.25
         do
             log_name="${l}.${w}.log"
-            echo "./train -k ${k} -l ${l} -t ${t} -r -1 -w ${w} -c 5 -p va item tr > ${logs_path}/${log_name}"
+            echo "./train -k ${k} -l ${l} -t ${t} -r -1 -w ${w} -c 5 --ns -p va item tr > ${logs_path}/${log_name}"
         done
     done
 }
